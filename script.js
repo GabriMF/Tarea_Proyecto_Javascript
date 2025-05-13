f1420944d94f665fd0441845dc461c538a5be404
Gobi <gabrimf25@gmail.com>
2025-05-13 22:22:42 +0200

    function added. Basic provisional styles also added

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function generadorExcusa(){
+const sujetos = [
+        "Mi perro",
+        "El vecino",
+        "Un alienígena",
+        "Mi abuela",
+        "Un pájaro gigante"
+    ];
 
-}
+    const acciones = [
+        "se comió",
+        "perdió",
+        "rompió",
+        "se llevó",
+        "mojó"
+    ];
+
+    const objetos = [
+        "mis deberes",
+        "mi ordenador",
+        "el trabajo que tenía hecho",
+        "la contraseña",
+        "el informe final"
+    ];
+
+    const motivos = [
+        "porque tenía hambre",
+        "por accidente",
+        "pensando que era suyo",
+        "para molestarme",
+        "porque estaba aburrido"
+    ];
+
+
+    function generadorExcusa() {
+        const s = sujetos[Math.floor(Math.random() * sujetos.length)];
+        const a = acciones[Math.floor(Math.random() * acciones.length)];
+        const o = objetos[Math.floor(Math.random() * objetos.length)];
+        const m = motivos[Math.floor(Math.random() * motivos.length)];
+
+        const excusa = `${s} ${a} ${o} ${m}.`;
+        document.getElementById("excusa").textContent = excusa;
+    }
